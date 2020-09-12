89773953820d1296a10f3160c4ed6aa63212626c
sweta sengupta <ssengupta801@gmail.com>
2020-09-11 21:22:50 -0500

    oop1 and oop2 completed

--- src/oop/oop1.py
+++ src/oop/oop1.py
@@ -17,3 +17,23 @@
 #     pass
 #
 # Put a comment noting which class is the base class
+
+class Vehicle:
+    # base class for groundvehicle and flightvehicle
+    pass
+
+class FlightVehicle(Vehicle):
+    # base class for airplane and starship
+    pass
+class Starship(FlightVehicle):
+    pass
+
+class GroundVehicle(Vehicle):
+    # base class for car and motorcycle
+    pass
+class Airplane(FlightVehicle):
+    pass
+class Car(GroundVehicle):
+    pass
+class Motorcycle(GroundVehicle):
+    pass
